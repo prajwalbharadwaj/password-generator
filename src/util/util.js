c329454ecd4d40da03c4bb035eb46d8849acab81
prajwalbharadwaj <prajwalrb17@gmail.com>
2025-10-25 18:17:22 +0530

    feat: updated util to have toast

--- src/util/util.js
+++ src/util/util.js
@@ -1,10 +1,16 @@
 import { ref } from "vue";
+import { toast } from "vue3-toastify";
+import "vue3-toastify/dist/index.css";
+
 const copied = ref(false);
 
 export async function useCopy(password) {
   if (password) {
     await navigator.clipboard.writeText(password);
     copied.value = true;
+    toast("Copied: " + password, {
+      autoClose: 1000
+    });
     setTimeout(() => (copied.value = false), 1500);
   }
 }
